221387c8d862f970a17f07172ec45c93595eb79a
Allen Lau <alau002@citymail.cuny.edu>
2023-04-05 21:25:10 -0400

    init; loop through folders, flatten images, append labels

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,6 +3,12 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import os
+import matplotlib.pyplot as plt 
+import numpy as np
+import re 
+from tqdm import tqdm 
+from time import sleep
 
 
 @click.command()
@@ -12,11 +18,36 @@ def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    special_labels = {'space':0,'nothing':1,'del':2}
+    raw_data = np.empty(shape=(120001,))
+    for subdir, dirs, files in os.walk(input_filepath):
+        print(subdir)
+        if len(dirs) != 0:
+            continue
+
+        for img in tqdm(files):
+            image = plt.imread(subdir+'/'+img)
+            if image.shape[2] == 3:
+                image = image.reshape(-1)
+            label = re.search('^(.*?)[^a-zA-Z]',img).group(0)[:-1]
+            if len(label)==1:
+                label = ord(label)
+            else:
+                label = special_labels[label]
+
+            image = np.append(image,label)
+            raw_data = np.vstack((raw_data,image))
+
+    print(raw_data.shape)
+
+
+
+    #logger = logging.getLogger(__name__)
+    #logger.info('making final data set from raw data')
 
 
 if __name__ == '__main__':
+    '''
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
@@ -26,5 +57,5 @@ if __name__ == '__main__':
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
-
+    '''
     main()
