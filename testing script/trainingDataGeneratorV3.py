4b674ad3f8340ef9b208e6c19ed48b20d5164599
skhanda001 <skhanda001@citymail.cuny.edu>
2023-05-08 23:05:27 -0400

    Updated the filename path with counter

--- testing script/trainingDataGeneratorV3.py
+++ testing script/trainingDataGeneratorV3.py
@@ -1,27 +1,29 @@
 # import libraries
 import cv2 as cv
 import mediapipe as mp
+import logging
+import numpy as np
 import os
 
 
 # function for detecting the hand
-def findHand(img):
-    # define mediapipe hands model
-    mpHands = mp.solutions.hands
-    # hands object to detect and track hands
-    hands = mpHands.Hands()
+
+
+def findHand(img, mp_model):
     # convert image to grayscale as preprocessing step
     g_img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
     # return detected hands
-    return hands.process(g_img)
+    return mp_model.process(g_img)
 
 
 # function for finding the rectangular bound of the hand detected
+
+
 def find_rectangle(img, results):
     # use the height and width of the img as a starting point for the x_min, y_min
     h, w, c = img.shape
     # constant to increase the size of the box
-    constant = 100
+    constant = 0
     # check if hand is detected via landmarks
     if results.multi_hand_landmarks:
         # loop through the detected hand landmarks (landmarks include x,y,z positions)
@@ -50,38 +52,32 @@ def find_rectangle(img, results):
 
 
 # function for getting a square image as input to the sign language interpreter model
+
+
 def get_cropped_image(img, x1, y1, x2, y2):
-    # define resolution constant for image preprocessing
-    res = (28, 28)
     # constant to add to build a square
-    constant = 200
-    # find the center of the rectangle
-    x, y = int((x2 + x1) / 2), int((y2 + y1) / 2)
-    # define slice bounds
-    y_l = y - constant
-    y_u = y + constant
-    x_l = x - constant
-    x_u = x + constant
-    # try to slice the numpy array
+    constant = 150
+    # #define slice bounds
+    y_l = y1 - constant
+    y_u = y2 + constant
+    x_l = x1 - constant
+    x_u = x2 + constant
+
+    # cropping image
     try:
         cropped_image = img[y_l:y_u, x_l:x_u]
-        # only return an image if it has a square resolution shape
-        if cropped_image.shape[0] != cropped_image.shape[1]:
-            cropped_image = None
-        else:
-            cropped_image = cv.resize(cropped_image, res)
     # catching index errors when slicing
     except IndexError:
         cropped_image = None
         print('Index Error')
     # return the cropped image
-    # resize the cropped image to 28x28 pixels
-
     return cropped_image
 
 
 # function for preprocessing image from video to conform to model input requirements
-def img_preprocessing(img, resolution):
+
+
+def img_preprocessing(img, resolution, type_str):
     # convert color of image to grayscale
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     # convert resolution of image
@@ -90,9 +86,9 @@ def img_preprocessing(img, resolution):
 
 
 # main function call
-def main():
+def sign_interpreter():
     label = 'A'
-    path = "./newTrainingData/" + label + "/"
+    path = "../data/external/newTrainingData/" + label + "/"
     if not os.path.exists(path):
         os.makedirs(path)
         print(f"Directory '{path}' created successfully")
@@ -100,11 +96,16 @@ def main():
         print(f"Directory '{path}' already exists")
 
     # CONSTANTS
-
+    # define resolution constant for image preprocessing
+    res = (28, 28)
     # define delay to get image from video feed every number of frames
-    interval = 0.1
+    interval = .5
     # laplacian filter variance threshold
-    lap_thres = 60
+    lap_thres = 20
+    # constant for rectangle spacing
+    rect_space = 100
+    # setting level to info to display logging messages
+    logging.basicConfig(level=logging.DEBUG)
 
     # define video capture from camera feed
     cap = cv.VideoCapture(0)
@@ -112,10 +113,15 @@ def main():
     fps = int(cap.get(cv.CAP_PROP_FPS))
     # frame count
     frame_count = 0
+    # define mediapipe hands model
+    mpHands = mp.solutions.hands
+    # hands object to detect and track hands
+    hands = mpHands.Hands()
 
     # check if video is unable to be obtained, and print message
     if cap.isOpened() == False:
         print('Error opening video stream')
+    count= 1
 
     # while video is being captured
     while (cap.isOpened()):
@@ -125,36 +131,33 @@ def main():
 
         if ret == True:
             # detect hands
-            detection_results = findHand(frame)
+            detection_results = findHand(frame, hands)
             # find rectangular bound around detected hand
             x1, y1, x2, y2 = find_rectangle(frame, detection_results)
 
             # if bound is found
-            if x1:
+            if x1 and y1 and x2 and y2:
                 # get cropped image for model input if possible
                 cropped_img = get_cropped_image(frame, x1, y1, x2, y2)
-                # crop28x28 = cropped_img.resize((28, 28))
-
                 # if cropped image is found
-                if cropped_img is not None:
+                if (cropped_img.shape[0] != 0) and cropped_img is not None:
                     # increment frame count
                     frame_count += 1
                     # logic for getting image every interval
                     if frame_count % (fps * interval) == 0:
-
                         # define laplacian filter to detect image if image is blurry (high variance = sharper image)
-                        laplacian = cv.Laplacian(frame, cv.CV_64F).var()
+                        laplacian = cv.Laplacian(cropped_img, cv.CV_64F).var()
+                        logging.debug('laplacian %s', laplacian)
                         if laplacian > lap_thres:
-                            # REPLACE CODE WITH IMREAD AND PASS INTO MODEL
-                            # cv.imshow('Cropped Image',img_preprocessing(cropped_img,res))
+                            filename = "cropped_image_{}.jpg".format(count)
 
-                            filename = "cropped_image_{}.jpg".format(frame_count)
-                            # Save the cropped image to local storage
                             cv.imwrite(path + filename, cropped_img)
+                            count +=1
                             print("File saved ", filename)
 
                 # draw rectangular bound in frame if found
-                cv.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv.rectangle(frame, (x1 - rect_space, y1 - rect_space),
+                             (x2 + rect_space, y2 + rect_space), (0, 255, 0), 2)
 
             # launch window and display
             cv.imshow('Sign Language Interpreter', frame)
@@ -173,4 +176,4 @@ def main():
 
 # entry
 if __name__ == '__main__':
-    main()
+    sign_interpreter()
