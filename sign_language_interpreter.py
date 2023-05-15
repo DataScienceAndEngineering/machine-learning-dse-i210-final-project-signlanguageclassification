34477e3d4a6f3637e7887c5b90abea2c63706dff
skhanda001 <skhanda001@citymail.cuny.edu>
2023-05-15 00:05:24 -0400

    clean the commented code

--- sign_language_interpreter.py
+++ sign_language_interpreter.py
@@ -3,13 +3,11 @@ from src.data import capture_video
 from src.features import features_pipeline
 from sklearn.pipeline import Pipeline
 from src.models import get_models
-import cv2 as cv
-import numpy as np
 import click
 
 
 @click.command()
-@click.option('--model', type=click.Choice(['NN-224x244', 'NN-28x28', 'PLACEHOLDER']), default='NN', required=True)
+@click.option('--model', type=click.Choice(['NN-224x244', 'NN-28x28', 'PLACEHOLDER']), default='NN-224x244', required=True)
 # main entry for sign_language_interpreter functionality
 def main(model):
     if model == 'NN-224x244':
